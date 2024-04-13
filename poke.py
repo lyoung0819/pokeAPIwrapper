81ba97177863cbe26bdc414c2ed34e6d7f553f0d
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-13 18:00:28 -0500

    ascii magic added

--- poke.py
+++ poke.py
@@ -1,16 +1,23 @@
 import requests
+from ascii_magic import AsciiArt
+
 
 class PokemonDeets:    
-    def __init__(self, name, height, weight):
+    
+    def __init__(self, name, height, weight, image):
         self.name = name.title()
         self.height = height
         self.weight = weight
+        self.image = image
 
     def __str__(self):
+        art = AsciiArt.from_url(self.image)
+        art_str = art.to_ascii()
         return f"""
         {self.name}: 
         height: {self.height}
         weight: {self.weight}
+        {art_str}
         """
     
 class PokemonAPI:
@@ -33,10 +40,11 @@ class PokemonAPI:
             name = pokemon_data.get('name')
             height = pokemon_data.get('height')
             weight = pokemon_data.get('weight')
-            poke_obj = PokemonDeets(name, height, weight) 
+            image = pokemon_data.get('sprites').get('front_default')
+            poke_obj = PokemonDeets(name, height, weight, image) 
             return poke_obj
         else:
-            print('I\'m sorry, we could not find data on {self.name}')
+            print(f'I\'m sorry, we could not find data on {poke_name}')
 
 
 
