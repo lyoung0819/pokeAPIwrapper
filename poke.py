0566cb95aed850d44d04fbb773c881bad90c6920
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-13 18:25:18 -0500

    so close to gaining access to the abilities... so far we can access the entire dictionary

--- poke.py
+++ poke.py
@@ -4,21 +4,23 @@ from ascii_magic import AsciiArt
 
 class PokemonDeets:    
     
-    def __init__(self, name, height, weight, image):
+    def __init__(self, name, height, weight, image, ability):
         self.name = name.title()
         self.height = height
         self.weight = weight
         self.image = image
+        self.ability = ability
 
     def __str__(self):
         art = AsciiArt.from_url(self.image)
         art_str = art.to_ascii()
         return f"""
-        {self.name}: 
-        height: {self.height}
-        weight: {self.weight}
-        {art_str}
-        """
+    {self.name}: 
+    height: {self.height}
+    weight: {self.weight}
+    abilities: {self.ability}
+    {art_str}
+    """
     
 class PokemonAPI:
     base_url = "https://pokeapi.co/api/v2/pokemon/"
@@ -31,7 +33,8 @@ class PokemonAPI:
             data = response.json() #make sure its json data
             return data
         else:
-            return None
+            return None 
+
 
     # method to get pokemon info
     def get_pokemon_deets(self, poke_name):
@@ -41,11 +44,12 @@ class PokemonAPI:
             height = pokemon_data.get('height')
             weight = pokemon_data.get('weight')
             image = pokemon_data.get('sprites').get('front_default')
-            poke_obj = PokemonDeets(name, height, weight, image) 
+            ability_place = pokemon_data.get('abilities')
+            ability = [ x['ability'] for x in ability_place ]
+            poke_obj = PokemonDeets(name, height, weight, image, ability) 
             return poke_obj
         else:
             print(f'I\'m sorry, we could not find data on {poke_name}')
-
 
 
 def main():
@@ -60,3 +64,15 @@ def main():
 
 
 main()
+
+
+# TRYING TO GET ABILITIES:
+#  ability = pokemon_data.get('abilities'[0][0]) // returns 'None'
+#  ability = pokemon_data.get('abilities'[0]) // returns 'None'
+#  ability = pokemon_data.get('abilities'[0])('name') // returns 'NoneType' object is not callable
+
+##
+#  ability_place = pokemon_data.get('abilities')
+#  ability = [ x['ability'] for x in ability_place ]
+#  RETURNS: 
+## [{'name': 'torrent', 'url': 'https://pokeapi.co/api/v2/ability/67/'}, {'name': 'rain-dish', 'url': 'https://pokeapi.co/api/v2/ability/44/'}]
