4f9851706f5a2de3c7f71fc5e4bfe28fb0c84acf
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-13 17:49:49 -0500

    working code - had to remove some extras, couldn't figure where it was going wrong

--- poke.py
+++ poke.py
@@ -1,54 +1,54 @@
-import requests # type: ignore
+import requests
 
 class PokemonDeets:    
-    def __init__(self, name, height, weight, abilities, stats):
-        self.name = name
+    def __init__(self, name, height, weight):
+        self.name = name.title()
         self.height = height
         self.weight = weight
-        self.abilities = abilities
-        self.stats = stats
 
     def __str__(self):
         return f"""
-        {self.name} : 
+        {self.name}: 
         height: {self.height}
         weight: {self.weight}
-        abilities: {self.abilities}
-        stats: {self.stats}"""
+        """
     
-
-
 class PokemonAPI:
     base_url = "https://pokeapi.co/api/v2/pokemon/"
     
     # setting up the get request blueprint of how to interact with the endpoint url 
     def __get(self, name):
-        self.name = name
-        request_url = f"{self.base_url}{self.name}"
+        request_url = self.base_url + name
         response = requests.get(request_url)
         if response.ok:
-            data = response.json()
+            data = response.json() #make sure its json data
             return data
         else:
             return None
-        
+
     # method to get pokemon info
-    def get_pokemon_deets(self, name):
-        pokemon_data = self.__get(name)
-        if pokemon_data:
-            height = pokemon_data['height']
-            weight = pokemon_data['weight']
-            abilities = pokemon_data['abilities']['abilities']['name']
-            stats = pokemon_data['stats']['base_stat']
+    def get_pokemon_deets(self, poke_name):
+        pokemon_data = self.__get(poke_name.lower())
+        if pokemon_data: #as long as there is data...
+            name = pokemon_data.get('name')
+            height = pokemon_data.get('height')
+            weight = pokemon_data.get('weight')
+            poke_obj = PokemonDeets(name, height, weight) 
+            return poke_obj
         else:
             print('I\'m sorry, we could not find data on {self.name}')
 
-    
+
+
 def main():
+    client = PokemonAPI()
     print('Welcome to our Pokemon database!')
-    client = PokemonAPI
-    name = input("Please enter your Pokemon's name you would like to search for:   ").lower()
-    poke_info = client.get_pokemon_deets(name)
-    print(poke_info)
+    while True:
+        poke = input('Please enter your Pokemon\'s name you would like to search for: ')
+        if poke == 'quit':
+            break
+        pokemon = client.get_pokemon_deets(poke)
+        print(pokemon)
+
 
 main()
